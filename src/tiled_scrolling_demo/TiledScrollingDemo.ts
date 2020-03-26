e118c8e202a10a33203470bb0b730273ac596150
Siyuan Zou <siyuanzou99@gmail.com>
2020-03-26 17:50:40 -0400

    Enemy bugs added

--- src/tiled_scrolling_demo/TiledScrollingDemo.ts
+++ src/tiled_scrolling_demo/TiledScrollingDemo.ts
@@ -28,9 +28,17 @@ game.getResourceManager().loadScene(DESERT_SCENE_PATH,
     let world : TiledLayer[] = game.getSceneGraph().getTiledLayers();
     let worldWidth : number = world[0].getColumns() * world[0].getTileSet().getTileWidth();
     let worldHeight : number = world[0].getRows() * world[0].getTileSet().getTileHeight();
-    for (let i = 0; i < 100; i++) {
-        let type : AnimatedSpriteType = game.getResourceManager().getAnimatedSpriteType("RED_CIRCLE_MAN");
-        let randomSprite : AnimatedSprite = new AnimatedSprite(type, "FORWARD");
+    for (let i = 0; i < 50; i++) {
+        let type : AnimatedSpriteType = game.getResourceManager().getAnimatedSpriteType("COCKROACH");
+        let randomSprite : AnimatedSprite = new AnimatedSprite(type, "IDLE");
+        let randomX : number = Math.random() * worldWidth;
+        let randomY : number = Math.random() * worldHeight;
+        randomSprite.getPosition().set(randomX, randomY, 0, 1);
+        game.getSceneGraph().addAnimatedSprite(randomSprite);
+    }
+    for (let i = 0; i < 50; i++) {
+        let type : AnimatedSpriteType = game.getResourceManager().getAnimatedSpriteType("DENKIMUSHI");
+        let randomSprite : AnimatedSprite = new AnimatedSprite(type, "IDLE");
         let randomX : number = Math.random() * worldWidth;
         let randomY : number = Math.random() * worldHeight;
         randomSprite.getPosition().set(randomX, randomY, 0, 1);
